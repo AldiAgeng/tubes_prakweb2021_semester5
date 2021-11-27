361c8371fd72cf434213ef0176e13d96e5f40a6e
bayucucanh <58686350+bayucucanh@users.noreply.github.com>
2021-11-27 15:43:52 +0700

    Membuat tampilan halaman index

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,11 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-    <div class="container">
-      <a class="navbar-brand" href="/">My Blog</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<div class="kotak"></div>
+  <nav class="navbar navbar-expand-lg navbar-light shadow-5-strong">
+    <div class="container-fluid mt-3 mb-1">
+      <a class="navbar-brand" href="/">Logo</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link {{ ($active === 'home') ? 'active' : '' }}" href="/">Home</a>
           </li>
@@ -13,11 +14,16 @@
             <a class="nav-link {{ ($active === 'posts') ? 'active' : '' }}" href="/posts">Blog</a>
           </li>
           <li class="nav-item">
-              <a class="nav-link {{ ($active === 'categories') ? 'active' : '' }}" href="/categories">Categories</a>
-            </li>
+            <a class="nav-link {{ ($active === 'categories') ? 'active' : '' }}" href="/categories">Categories</a>
+          </li>
         </ul>
-        
-        <ul class="navbar-nav ms-auto">
+        <form class="d-flex search">
+          <input class="form-control rounded-pill bg-dark col-search border border-dark text-light" type="search" placeholder="Search" aria-label="Search">
+        </form>
+
+        {{-- Login --}}
+
+        {{-- <ul class="navbar-nav ms-auto">
           @auth
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -39,8 +45,7 @@
             <a href="/login" class="nav-link  {{ ($active === 'login') ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
           </li>
           @endauth
-        </ul>
-
+        </ul> --}}
       </div>
     </div>
   </nav>
