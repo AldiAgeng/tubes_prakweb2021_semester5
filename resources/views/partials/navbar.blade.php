6259caa91faa9836f96630078e8be54584b0ae1b
AldiAgeng <aldiageng48@gmail.com>
2021-11-29 09:13:48 +0700

    Memperbaiki search di halaman depan

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -17,35 +17,17 @@
             <a class="nav-link {{ Request::is('login*') ? 'active' : '' }}" href="/login">Login</a>
           </li>
         </ul>
-        <form class="d-flex search">
-          <input class="form-control rounded-pill bg-dark col-search border border-dark text-light" type="search" placeholder="Search" aria-label="Search">
+        <form action="/posts" class="d-flex search">
+          
+          @if (request('category'))
+          <input type="hidden" name="category" value="{{ request('category') }}">
+          @endif
+          @if (request('author'))
+          <input type="hidden" name="author" value="{{ request('author') }}">
+          @endif
+
+          <input class="form-control rounded-pill bg-dark col-search border border-dark text-light" type="search" placeholder="Search..." aria-label="Search" name="search" value="{{ request('search') }}">
         </form>
-
-        {{-- Login --}}
-
-        {{-- <ul class="navbar-nav ms-auto">
-          @auth
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Welcome back, {{ auth()->user()->name }}
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href=/dashboard><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li>
-                <form action="/logout" method="post">
-                  @csrf
-                  <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout </button>
-                </form>
-              </li>
-            </ul>
-          </li>
-          @else
-          <li class="navbar-item">
-            <a href="/login" class="nav-link  {{ ($active === 'login') ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
-          </li>
-          @endauth
-        </ul> --}}
       </div>
     </div>
   </nav>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,25 +2,6 @@
 @section('container')
 <h1 class="mb-3 text-center">{{ $title }}</h1>
 
-<div class="row justify-content-center mb-3">
-  <div class="col-md-6">
-    <form action="/posts">
-
-      @if (request('category'))
-          <input type="hidden" name="category" value="{{ request('category') }}">
-      @endif
-
-      @if (request('author'))
-          <input type="hidden" name="author" value="{{ request('author') }}">
-      @endif
-      
-      {{-- <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Search..." name="search" value="{{ request('search') }}">
-        <button class="btn btn-outline-danger" type="submit">Search</button>
-      </div> --}}
-    </form>
-  </div>
-</div>
 
 @if($posts->count())
 <div class="container">
