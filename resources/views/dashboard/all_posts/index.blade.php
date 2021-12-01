18e33ebc7053a800913307332d7067a71766aa72
Moh_RizkiRamdani <60476092+mohamadrizki725@users.noreply.github.com>
2021-12-01 14:42:26 +0700

    membuat edit blade untuk allpost

--- resources/views/dashboard/all_posts/index.blade.php
+++ resources/views/dashboard/all_posts/index.blade.php
@@ -32,7 +32,9 @@
                       <td>{{ $post->category->name }}</td>
                       <td>
                         <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
-                        <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+                        
+                        <a href="/dashboard/all_posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+
                         <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
                           @method('delete')
                           @csrf
